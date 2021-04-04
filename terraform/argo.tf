163abac54491518d539dd1a0635ca6d0534c3c13
ghilbut <ghilbut@gmail.com>
2021-04-04 10:03:01 +0900

    Remove resource.kubernetes_manifest.apps from terraform (#10)

--- terraform/argo.tf
+++ terraform/argo.tf
@@ -24,89 +24,33 @@ resource helm_release argo {
     ]
   }
 
-  name       = "argo"
-  chart      = "argo-cd"
-  repository = "https://argoproj.github.io/argo-helm/"
-  version    = "2.13.0"
-  namespace  = kubernetes_namespace.argo.metadata[0].name
-
-  values = [
-    file("${path.module}/helm-chart/argo/values.yaml"),
-  ]
+  name      = "argo"
+  chart     = "../helm/operator/argo/"
+  dependency_update = true
+  namespace = kubernetes_namespace.argo.metadata[0].name
 
   set {
-    name  = "fullnameOverride"
-    value = "argo-argocd"
+    name  = "domain"
+    value = var.domain
   }
 
   set {
-    name  = "server.config.url"
-    value = "http://${local.argo_host}"
-  }
-
-  set {
-    name  = "server.ingress.hosts[0]"
+    name  = "cd.server.ingress.hosts[0]"
     value = local.argo_host
   }
 
+  set {
+    name  = "cd.server.config.url"
+    value = "http://${local.argo_host}"
+  }
+
   set_sensitive {
-    name  = "configs.secret.argocdServerAdminPassword"
+    name  = "cd.configs.secret.argocdServerAdminPassword"
     value = data.external.argo.result.encpw
   }
 
   set {
-    name  = "configs.secret.argocdServerAdminPasswordMtime"
+    name  = "cd.configs.secret.argocdServerAdminPasswordMtime"
     value = data.external.argo.result.mtime
-  }
-}
-
-resource kubernetes_manifest apps {
-  provider = kubernetes-alpha
-
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "Application"
-
-    metadata = {
-      name      = "apps"
-      namespace = kubernetes_namespace.argo.metadata[0].name
-      labels    = {
-        "argo.okmvp.internal/category" = "operator"
-      }
-    }
-
-    spec = {
-      project     = "default"
-      source      = {
-        repoURL        = "https://github.com/okmvp/docker-desktop-k8s.git"
-        targetRevision = "main"
-        path           = "apps/"
-        helm           = {
-          parameters = [
-            {
-              name  = "domain"
-              value = var.domain
-            },
-          ]
-          valueFiles = [
-            "values.yaml",
-          ]
-          version    = "v2"
-        } 
-      }
-      destination = {
-        server    = "https://kubernetes.default.svc"
-        namespace = kubernetes_namespace.argo.metadata[0].name
-      }
-      syncPolicy  = {
-        automated = {
-          prune    = true
-          selfHeal = true
-        }
-        syncOptions = [
-          "Validate=true",
-        ]
-      }
-    }
   }
 }
